085a5e52d198feaf0b65aaf8bfc27abfcf0bf672
Malphice <matt.schuecker@gmx.at>
2024-10-01 01:31:20 +0200

    Adjust CORS origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(TaskModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://tasks-fe.vercel.app',
     methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
   });
   await app.listen(3000);
