e80cd5a980ded3feddb0cd36778ec095e4fa02be
Malphice <matt.schuecker@gmx.at>
2024-09-30 22:00:27 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { TaskModule } from './TaskModule';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(TaskModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
+  });
   await app.listen(3000);
 }
+
 bootstrap();
